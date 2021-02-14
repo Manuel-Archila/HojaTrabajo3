facb7d7f1102a6122125a5b87add9820d22f6991
jwlh00 <juunshuu00@gmail.com>
2021-02-14 16:52:04 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,26 +7,155 @@ public class Main {
     Scanner scan = new Scanner(System.in);
     Sorts sort = new Sorts();
 
-    System.out.println("Cuanto datos desea crear");
+    boolean menuPrincipal = true;
     boolean s = true;
     int cant =0;
-    while(s)
-    try{
-      cant = scan.nextInt();
-      if(cant<=3000 && cant>0){
-        s = false;
+
+    while(menuPrincipal){
+      try{
+        System.out.println("\nQue tipo de sort quiere utilizar?");
+        System.out.println("1. Gnome Sort");
+        System.out.println("2. Merge Sort");
+        System.out.println("3. Quick Sort");
+        System.out.println("4. Radix Sort");
+        System.out.println("5. Selection Sort");
+        System.out.println("6. Salir");
+
+        int seleccionMenu = scan.nextInt();
+
+        switch(seleccionMenu){
+          case 1:{
+            s = true;
+            while(s){
+              try{
+                System.out.println("Cuanto datos desea crear");
+                cant = scan.nextInt();
+                if(cant<=3000 && cant>0){
+                  s = false;
+                }
+              }catch(Exception e){
+                System.out.println("Ingrese valores numericos");
+              }
+            }
+            Integer[] arra = gen.Generar(cant);
+            System.out.println("El Array Original es:");
+            System.out.println(Arrays.toString(arra));
+            sort.gnomeSort(arra);
+            System.out.println("El Array Sorteado utilizando el algoritmo Gnome sort es:");
+            System.out.println(Arrays.toString(arra));
+
+            break;
+
+          }
+          case 2:{
+            s = true;
+            while(s){
+              try{
+                System.out.println("Cuanto datos desea crear");
+                cant = scan.nextInt();
+                if(cant<=3000 && cant>0){
+                  s = false;
+                }
+              }catch(Exception e){
+                System.out.println("Ingrese valores numericos");
+              }
+            }
+            Integer[] arra = gen.Generar(cant);
+            System.out.println("El Array Original es:");
+            System.out.println(Arrays.toString(arra));
+            arra = sort.mergeSort(arra);
+            System.out.println("El Array Sorteado utilizando el algoritmo Merge sort es:");
+            System.out.println(Arrays.toString(arra));
+
+            break;
+
+          }
+          case 3:{
+            s = true;
+            while(s){
+              try{
+                System.out.println("Cuanto datos desea crear");
+                cant = scan.nextInt();
+                if(cant<=3000 && cant>0){
+                  s = false;
+                }
+              }catch(Exception e){
+                System.out.println("Ingrese valores numericos");
+              }
+            }
+            Integer[] arra = gen.Generar(cant);
+            System.out.println("El Array Original es:");
+            System.out.println(Arrays.toString(arra));
+            //Poner para llamar al metodo de quick sort
+            System.out.println("El Array Sorteado utilizando el algoritmo Quick sort es:");
+            System.out.println(Arrays.toString(arra));
+
+            break;
+
+          }
+          case 4:{
+            s = true;
+            while(s){
+              try{
+                System.out.println("Cuanto datos desea crear");
+                cant = scan.nextInt();
+                if(cant<=3000 && cant>0){
+                  s = false;
+                }
+              }catch(Exception e){
+                System.out.println("Ingrese valores numericos");
+              }
+            }
+            Integer[] arra = gen.Generar(cant);
+            System.out.println("El Array Original es:");
+            System.out.println(Arrays.toString(arra));
+            sort.radixsort(arra, cant-1);
+            System.out.println("El Array Sorteado utilizando el algoritmo Radix sort es:");
+            System.out.println(Arrays.toString(arra));
+
+            break;
+
+          }
+          case 5:{
+            s = true;
+            while(s){
+              try{
+                System.out.println("Cuanto datos desea crear");
+                cant = scan.nextInt();
+                if(cant<=3000 && cant>0){
+                  s = false;
+                }
+              }catch(Exception e){
+                System.out.println("Ingrese valores numericos");
+              }
+            }
+            Integer[] arra = gen.Generar(cant);
+            System.out.println("El Array Original es:");
+            System.out.println(Arrays.toString(arra));
+            sort.selectionSort(arra);
+            System.out.println("El Array Sorteado utilizando el algoritmo Selection sort es:");
+            System.out.println(Arrays.toString(arra));
+
+            break;
+
+          }
+          case 6:{
+            System.out.println("Gracias por utilizar nuestro programa");
+            menuPrincipal = false;
+            break;
+
+          }
+          default:{
+            scan.nextLine();
+            System.out.println("Se ingreso una opcion invalida, intentelo de nuevo.");
+          }
+        }
+      } catch (Exception e) {
+        scan.nextLine();
+        System.err.println("Se ingreso una opcion invalida, intentelo de nuevo");
       }
-    }catch(Exception e){
-      System.out.println("Ingrese valores numericos");
     }
-    Integer[] arra = gen.Generar(cant);
 
-    sort.radixsort(arra, cant-1);
-
-    System.out.println(Arrays.toString(arra));
-
-    scan.close();
-    
   }
   
 }
